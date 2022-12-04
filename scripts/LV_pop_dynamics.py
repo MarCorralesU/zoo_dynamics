0c59d28fe87478ddb5e9e74565a4a6abdb15582b
MarCorralesU <marco.corrales@noaa.gov>
2022-12-03 21:50:11 -0500

    initial script commit

---
+++ scripts/LV_pop_dynamics.py
@@ -0,0 +1,5 @@
+# Lokda Volterra Dynamics adapter after Miller & Wheeler (2012): Biological Oceanography
+import numpy as np
+import pandas as pd
+# create dataframe with two columns, for predator and prey population size
+LV_df = pd.DataFrame()
